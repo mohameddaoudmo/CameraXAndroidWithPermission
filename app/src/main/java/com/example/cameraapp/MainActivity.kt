af79e77f1b440971e90806f51aae474a9c654d6e
mohamed taha93 <mohamedtahadawoud@gmail.com>
2024-02-12 15:00:32 +0200

    commit

---
+++ app/src/main/java/com/example/cameraapp/MainActivity.kt
@@ -0,0 +1,187 @@
+package com.example.cameraapp
+
+import MainScreen
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cameraapp.someofUi.CameraPermissionTextProvider
+import com.example.cameraapp.someofUi.PermissionDialog
+import com.example.cameraapp.someofUi.RecordAudioPermissionTextProvider
+import com.example.cameraapp.someofUi.cameraPerview
+import com.example.cameraapp.ui.theme.CameraAppTheme
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+class MainActivity : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CameraAppTheme {
+                val viewModel = viewModel<MainViewModel>()
+                val cameraViewModel = viewModel<CameraViewModel>()
+
+
+                val dialogQueue = viewModel.visiblePermissionDialogQueue
+
+
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    MyApp(viewModel,cameraViewModel)
+
+                }
+
+
+            }
+
+
+        }
+    }
+
+
+    companion object {
+        val CAMERAX_PERMISSIONS = arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO,
+        )
+    }
+
+
+    @Composable
+    fun MyApp(viewModel: MainViewModel,cameraViewModel: CameraViewModel) {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "screen1") {
+            composable("screen1") { FristScreen(navController, viewModel) }
+            composable("screen2") { MainScreen(navController, cameraViewModel) }
+        }
+    }
+
+    @Composable
+    fun FristScreen(navController: NavController, viewModel: MainViewModel) {
+        // Your Composable content for Screen1
+        val dialogQueue = viewModel.visiblePermissionDialogQueue
+
+        val multiplePermissionResultLauncher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestMultiplePermissions(),
+            onResult = { perms ->
+                MainActivity.CAMERAX_PERMISSIONS.forEach { permission ->
+                    viewModel.onPermissionResult(
+                        permission = permission,
+                        isGranted = perms[permission] == true
+                    )
+                    if (perms[Manifest.permission.CAMERA] == true && perms[Manifest.permission.RECORD_AUDIO] == true) {
+                        navController.navigate("screen2")
+
+                    }
+
+
+                }
+            }
+        )
+
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = {
+
+            }) {
+                Text(text = "Request one permission")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+                multiplePermissionResultLauncher.launch(CAMERAX_PERMISSIONS)
+            }) {
+                Text(text = "Request multiple permission")
+            }
+        }
+
+        dialogQueue
+            .reversed()
+            .forEach { permission ->
+                PermissionDialog(
+                    permissionTextProvider = when (permission) {
+                        Manifest.permission.CAMERA -> {
+                            CameraPermissionTextProvider()
+                        }
+
+                        Manifest.permission.RECORD_AUDIO -> {
+                            RecordAudioPermissionTextProvider()
+                        }
+
+
+                        else -> return@forEach
+                    },
+                    isPermanentlyDeclined = !shouldShowRequestPermissionRationale(
+                        permission
+                    ),
+                    onDismiss = viewModel::dismissDialog,
+                    onOkClick = {
+                        viewModel.dismissDialog()
+                        multiplePermissionResultLauncher.launch(
+                            arrayOf(permission)
+                        )
+                    },
+                    onGoToAppSettingsClick = ::openAppSettings
+                )
+            }
+
+    }
+
+
+
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CameraAppTheme {
+    }
+}
+
+fun Activity.openAppSettings() {
+    Intent(
+        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+        Uri.fromParts("package", packageName, null)
+    ).also(::startActivity)
+}
---
+++ app/src/main/java/com/example/cameraapp/someofUi/FristScreen.kt
@@ -0,0 +1,205 @@
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture.OnImageCapturedCallback
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cameraswitch
+import androidx.compose.material.icons.filled.Photo
+import androidx.compose.material.icons.filled.PhotoCamera
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.currentCompositionLocalContext
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import com.example.cameraapp.CameraViewModel
+import com.example.cameraapp.MainViewModel
+import com.example.cameraapp.someofUi.cameraPerview
+import kotlinx.coroutines.launch
+
+private fun takePhoto (controller: LifecycleCameraController,onPhotoTake: (Bitmap)->Unit,context:Context){
+    controller.takePicture(
+        ContextCompat.getMainExecutor(context),
+        object : OnImageCapturedCallback() {
+
+
+
+            override fun onError(exception: ImageCaptureException) {
+                super.onError(exception)
+            }
+
+            override fun onCaptureSuccess(image: ImageProxy) {
+                super.onCaptureSuccess(image)
+                val matrix = Matrix().apply {
+                    postRotate(image.imageInfo.rotationDegrees.toFloat())
+                }
+                val rotatedBitmap = Bitmap.createBitmap(
+                    image.toBitmap(),
+                    0,
+                    0,
+                    image.width,
+                    image.height,
+                    matrix,
+                    true
+                )
+
+                onPhotoTake(rotatedBitmap)
+
+            }
+        }
+    )
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(navController: NavController, viewModel: CameraViewModel) {
+
+    val scoffeldState = rememberBottomSheetScaffoldState()
+    val scope = rememberCoroutineScope()
+
+
+    val context = LocalContext.current
+    val controller = remember {
+        LifecycleCameraController(context).apply {
+            setEnabledUseCases(
+                CameraController.IMAGE_CAPTURE or CameraController.VIDEO_CAPTURE
+            )
+        }
+
+    }
+    val bitmap by viewModel.bitmaps.collectAsState()
+
+    BottomSheetScaffold(scaffoldState = scoffeldState, sheetPeekHeight = 0.dp, sheetContent = {PhotoBottomSheetContent(bitmaps = bitmap,)}) {
+
+        Box(modifier = Modifier.padding(it)) {
+
+            cameraPerview(controller = controller, modifier = Modifier.fillMaxSize())
+            IconButton(
+                onClick = {
+                    controller.cameraSelector =
+                        if (controller.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) {
+                            CameraSelector.DEFAULT_FRONT_CAMERA
+                        } else {
+                            CameraSelector.DEFAULT_BACK_CAMERA
+                        }
+
+                },
+                modifier = Modifier.offset {
+                    IntOffset(
+                        16.dp.toPx().toInt(),
+                        16.dp.toPx().toInt()
+                    )
+                }) {
+                Icon(imageVector = Icons.Default.Cameraswitch, contentDescription = null)
+
+            }
+            Row(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                IconButton(
+                    onClick = {
+                        scope.launch {
+                            scoffeldState.bottomSheetState.expand()
+                        }
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Photo,
+                        contentDescription = "Open gallery"
+                    )
+                }
+                IconButton(
+                    onClick = {
+                        takePhoto(
+                            controller = controller,
+                            onPhotoTake = viewModel::onTakePhoto
+                            ,context = context
+                        )
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.PhotoCamera,
+                        contentDescription = "Take photo"
+                    )
+                }
+
+            }
+        }
+
+
+
+    }
+
+
+}
+@Composable
+fun PhotoBottomSheetContent(
+    bitmaps: List<Bitmap>,
+    modifier: Modifier = Modifier
+) {
+    if(bitmaps.isEmpty()) {
+        Box(
+            modifier = modifier
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("There are no photos yet")
+        }
+    } else {
+        LazyVerticalStaggeredGrid(
+            columns = StaggeredGridCells.Fixed(2),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalItemSpacing = 16.dp,
+            contentPadding = PaddingValues(16.dp),
+            modifier = modifier
+        ) {
+            items(bitmaps){
+                Image(
+                    bitmap =it.asImageBitmap(),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(10.dp))
+                )
+            }
+
+        }
+    }
+}
